87487b2d6e98d88a9b699dc6c45948712d8d93b1
Dilmurod Ubaydullaev <dilmurodubaydullaev22@gmail.com>
2019-12-26 11:10:57 +0500

    Add navigation controller and title

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Dilmurod Ubaydullaev on 12/26/19.
+//  Copyright © 2019 Dilmurod Ubaydullaev. All rights reserved.
+//
+
+import Foundation
+
+class ToDo{
+    var name = ""
+    var important = false
+}
